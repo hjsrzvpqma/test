c8c641939673220c03c6df9ab9241d8f772a250e
MAKANxyz <makan.j9090@gmail.com>
2016-03-07 16:09:39 +0330

    Create ver.lua

---
+++ plugins/ver.lua
@@ -0,0 +1,25 @@
+do
+
+function run(msg, matches)
+  return 'CycloneTg
+
+ '.. VERSION .. [[ 
+  
+
+Best of antispam bots V5 an advanced by telegram-bot! 
+
+For buy groups please send nerkh to this group or review to bot mods!
+
+GoodLuck : ).]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows bot version",
+  patterns = {
+    "^ver$"
+  }, 
+  run = run 
+}
+
+end
