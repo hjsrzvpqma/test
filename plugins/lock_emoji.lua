bcaacf389a298ac80e1a94e5fae5cf8f52564395
hjsrzvpqma <makan.j100100@gmail.com>
2016-03-08 23:18:54 +0330

    Update lock_emoji.lua

--- plugins/lock_emoji.lua
+++ plugins/lock_emoji.lua
@@ -1,7 +1,11 @@
 local function run(msg)
     if msg.to.type == 'chat' and not is_momod(msg) then
         chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-        return فرستادن اموجی ممنوع است'
+        return 'Emoji is not allowed here⛔️
+local function run(msg)
+    if msg.to.type == 'chat' and not is_momod(msg) then
+        chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+        return Emoji is not allowed here⛔️'
     end
 end
 
